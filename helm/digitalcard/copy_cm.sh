a774711d351b6556e8a9d1ea53361d8e9cc81f47
gaganamadival <94885434+gaganamadival@users.noreply.github.com>
2023-05-19 10:52:50 +0530

    [MOSIP-25858] Added helm-chart and deployment script for digital repo (#35)
    * [MOSIP-25858] Added helm-chart and deployment script for digital repo
    
    * [MOSIP-25858] Added helm-chart and deployment script for digital repo
    
    * [MOSIP-25858] Added helm-chart and deployment script for digital repo
    
    * [MOSIP-25858] Added helm-chart and deployment script for digital repo

---
+++ helm/digitalcard/copy_cm.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Copy configmaps from other namespaces
+# DST_NS: Destination (current) namespace
+COPY_UTIL=./copy_cm_func.sh
+DST_NS=digitalcard
+
+$COPY_UTIL configmap global default $DST_NS
+$COPY_UTIL configmap artifactory-share artifactory $DST_NS
+$COPY_UTIL configmap config-server-share config-server $DST_NS
